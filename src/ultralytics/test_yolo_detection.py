279763df75d7d5f1bf1a28b4aace0992142eb9fb
Silvan Stadelmann <silvasta3@gmail.com>
2025-05-25 22:34:25 +0200

    yolo11n run until convergence

--- src/ultralytics/test_yolo_detection.py
+++ src/ultralytics/test_yolo_detection.py
@@ -3,14 +3,6 @@ from utils import load_device_print_information
 
 device = load_device_print_information()
 
-model = YOLO("yolo11n.pt")
-# model = YOLO("models/yolo11n.pt").to(device)
-# model = YOLO("/home/silvan/ultralytics/models/yolo11n.pt")
-# model = YOLO("/home/silvan/Coding/mlmc-project-2025/src/ultralytics/models/yolo11n.pt")
-# print(model)
-
-### params
-
 # dataset_name = "african-wildlife"
 # dataset_name = "coco8"
 # dataset_name = "coco128"
@@ -20,32 +12,39 @@ dataset_name = "VisDrone"
 
 data = f"{dataset_name}.yaml"
 
-experiment_name = "initial_setup"
+project_name = "test_yolo"
+experiment_name = "train_n_to_convergence"
 
 optimizer = "auto"  # SGD, Adam, AdamW, NAdam, RAdam, RMSProp
 
-### train
-results = model.train(
-    data=data,
-    device=device,
-    epochs=5,
-    # time = None,
-    patience=50,  # 100
-    # batch=16,
-    # imgsz=640,
-    # cache = False,
-    project=f"test_yolo/{dataset_name}",
-    name=experiment_name,
-    optimizer=optimizer,
-    # classes=None,
-    # resume=False,
-    # fraction=1.0,
-    # val=True,
-    plots=True,
-)
+resume = False
+resume = True
+if resume:
+    model_path = f"{project_name}/{dataset_name}/{experiment_name}/weights/last.pt"
+    model = YOLO(model_path)
+    ### train
+    results = model.train(resume=resume)
+else:
+    model_path = "yolo11n.pt"
+    model = YOLO(model_path)
+    ### train
+    results = model.train(
+        data=data,
+        device=device,
+        epochs=500,
+        # time = None,
+        patience=20,  # 100,
+        # batch=16,
+        # imgsz=640,
+        # cache = False,
+        project=f"{project_name}/{dataset_name}",
+        name=experiment_name,
+        optimizer=optimizer,
+        # classes=None,
+        # resume=resume,  # False,
+        # fraction=1.0,
+        # val=True,
+        plots=True,
+    )
 
-# print()
-# print("Results")
-# print({results})
-# print()
 print("Training done")
