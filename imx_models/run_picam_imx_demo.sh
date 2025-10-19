29d1ec097c128cf548a7e6cd7edeeb009bf706d5
silvan <silvasta3@gmail.com>
2025-10-19 20:19:22 +0200

    clean up

--- imx_models/run_picam_imx_demo.sh
+++ imx_models/run_picam_imx_demo.sh
@@ -2,14 +2,22 @@
 
 # choose the desired demo task
 
-DEMO_FILE=imx500_object_detection_demo.py
-# DEMO_FILE=imx500_object_detection_demo_mp.py
+# DEMO_FILE=imx500_object_detection_demo.py
+DEMO_FILE=imx500_object_detection_demo_mp.py
 
 # set the model
-MODEL=yolo_n_1/network.rpk
+# MODEL=yolo_n_1/network.rpk
+# ### working
+# MODEL=n_to_convergence_all-2025-06-23_03-34-40/network.rpk
+# MODEL=n_640/network.rpk # bad
+# ### testing
+# MODEL=n_to_convergence_all-2025-06-23_03-38-14/network.rpk
+# ### problem
+MODEL=s_to_convergence_all-2025-06-23_03-49-35/network.rpk
+# MODEL=s_to_convergence-2025-06-23_03-41-47/network.rpk
 
 # choose frames or pass as argument
-FRAMES=${1:-17} # default=17
+FRAMES=${1:-10} # default=17
 
 # class labels for dataset
 LABELS=labels.txt
